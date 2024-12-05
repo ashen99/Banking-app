f8248d90dc5d2a230a0a6f4301363c8500eff827
ashen99 <ashaneperera1999@gmail.com>
2024-12-05 22:39:43 +0530

    Initialize project

---
+++ src/main/java/com/ashen/testing_bank_app/TestingBankAppApplication.java
@@ -0,0 +1,13 @@
+package com.ashen.testing_bank_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingBankAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingBankAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ashen/testing_bank_app/TestingBankAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashen.testing_bank_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingBankAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
