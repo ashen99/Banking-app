c3613becf8eb0053f31f238f3613abe9d669ba80
ashen99 <ashaneperera1999@gmail.com>
2024-12-29 23:57:02 +0530

    Add swagger implementation

--- src/main/java/com/ashen/testing_bank_app/controller/UserController.java
+++ src/main/java/com/ashen/testing_bank_app/controller/UserController.java
@@ -2,21 +2,42 @@ package com.ashen.testing_bank_app.controller;
 
 import com.ashen.testing_bank_app.dto.*;
 import com.ashen.testing_bank_app.service.impl.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
+@Tag(name = "User Account Management APIs")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
+    @Operation(
+            summary = "Create new user account",
+            description = "Creating a new user and assigning an account ID"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "Http Status 201 CREATED"
+    )
     @PostMapping("/add-user")
     public BankResponse createAccount(@RequestBody UserRequest userRequest) {
         return userService.createAccount(userRequest);
     }
 
+    @Operation(
+            summary = "Balance Enquiry",
+            description = "Given an account number, check the balance of the account"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "Http Status 201 SUCCESS"
+    )
     @GetMapping("/balanceEnquiry")
     public BankResponse getBalanceEnquiry(@RequestBody EnquiryRequest enquiryRequest) {
         return userService.balanceEnquiry(enquiryRequest);
